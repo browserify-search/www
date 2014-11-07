52c72a727b07164e043ee9af6b5a98a43dbdb91a
Toby Ho <airportyh@gmail.com>
2014-11-07 11:11:15 -0500

    I removed the semicolons ;)

--- app.js
+++ app.js
@@ -1,48 +1,48 @@
-var express = require('express');
-var stylish = require('stylish');
-var favicon = require('static-favicon');
-var browserify_file = require('browserify-file');
-var taters = require('taters');
-var hbs = require('hbs');
-var debug = require('debug')('browserify-search:www');
-var paginator = require('./lib/paginator');
-var search = require('./lib/search');
-var escape = require('escape-html');
-var corsify = require('corsify');
+var express = require('express')
+var stylish = require('stylish')
+var favicon = require('static-favicon')
+var browserify_file = require('browserify-file')
+var taters = require('taters')
+var hbs = require('hbs')
+var debug = require('debug')('browserify-search:www')
+var paginator = require('./lib/paginator')
+var search = require('./lib/search')
+var escape = require('escape-html')
+var corsify = require('corsify')
 
-hbs.registerHelper('paginate', paginator);
+hbs.registerHelper('paginate', paginator)
 hbs.registerHelper('score', function(number){
-	return (10 * number).toFixed(0);
-});
+	return (10 * number).toFixed(0)
+})
 
 // we set certain settings based on production or not
-var kProduction = process.env.NODE_ENV === 'production';
+var kProduction = process.env.NODE_ENV === 'production'
 
-var app = express();
+var app = express()
 
-app.set('views', __dirname + '/views');
-app.set('view engine', 'html' );
+app.set('views', __dirname + '/views')
+app.set('view engine', 'html' )
 app.set('view options', {
 	cache: kProduction,
 	layout: false
-});
-app.engine('html', hbs.__express);
+})
+app.engine('html', hbs.__express)
 
-//app.use(favicon(__dirname + '/static/favicon.ico'));
+//app.use(favicon(__dirname + '/static/favicon.ico'))
 
 app.use(taters({
 	cache: kProduction
-}));
+}))
 app.use(stylish({
 	src: __dirname + '/public/',
 	compress: kProduction
-}));
+}))
 
-app.use(express.static(__dirname + '/static'));
+app.use(express.static(__dirname + '/static'))
 
 app.get('/', function(req, res, next) {
-	res.render('index');
-});
+	res.render('index')
+})
 
 app.get('/search', searchMethod(function(results, query, pageOptions, res){
 	res.render('search', {
@@ -50,8 +50,8 @@ app.get('/search', searchMethod(function(results, query, pageOptions, res){
 		total: results.total,
 		results: results.hits,
 		pageOptions: pageOptions
-	});
-}));
+	})
+}))
 
 app.get('/api/search', corsify({
 	 "Access-Control-Allow-Methods": "GET"
@@ -62,51 +62,50 @@ app.get('/api/search', corsify({
 		pageSize: pageOptions.pageSize,
 		total: results.total,
 		hits: results.hits
-	});
-})));
+	})
+})))
 
 function searchMethod(render){
 	return function(req, res, next) {
 
-		var query = req.query.q;
-		var page = Number(req.query.page || 1);
-		var pageSize = Number(req.query.pageSize || 20);
+		var query = req.query.q
+		var page = Number(req.query.page || 1)
+		var pageSize = Number(req.query.pageSize || 20)
 		if (pageSize > 100){
-			pageSize = 100;
+			pageSize = 100
 		}
-		var accept = req.headers['Accept'];
 
 		var pageOptions = {
 			page: page,
 			pageSize: pageSize
-		};
+		}
 
 		search(query, pageOptions, function(err, results) {
 			if (err) {
-				return next(err);
+				return next(err)
 			}
-			pageOptions.total = results.total;
-			pageOptions.numPages = Math.ceil(results.total / pageSize);
-			var hits = results.hits;
+			pageOptions.total = results.total
+			pageOptions.numPages = Math.ceil(results.total / pageSize)
+			var hits = results.hits
 
 			hits.forEach(function(hit){
-				hit.name = escape(hit.name);
-				hit.description = escape(hit.description);
-			});
+				hit.name = escape(hit.name)
+				hit.description = escape(hit.description)
+			})
 
-			render(results, query, pageOptions, res);
-		});
+			render(results, query, pageOptions, res)
+		})
 
 	}
 }
 
 app.use(function(req, res, next) {
-	res.render('404');
-});
+	res.render('404')
+})
 
 // error handler
 app.use(function(err, req, res, next) {
-	console.log(err.stack);
-});
+	console.log(err.stack)
+})
 
-module.exports = app;
+module.exports = app
